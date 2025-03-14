1370ee1e7f5ea992c2e350849ac6213cd5355aa2
E-RYJ <ej.aacount@gmail.com>
2025-03-14 14:29:58 -0700

    endpoint para registrar nuevos admistradores añadido

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -5,6 +5,8 @@ const { isAdmin } = require('../middlewares/admin.middleware');
 const { getProduct, addProduct, modifyProductDetails, getAllProductsPagination } = require('../controllers/product.controller');
 const { toggleProductStatus, updateProductCustomization, uploadProductImage } = require('../controllers/admin.controller');
 const { handleProductImageUpload } = require('../middlewares/upload.middleware');
+const { validateRegister } = require('../middlewares/validateInput');
+const { registerAdmin } = require('../controllers/auth.controller');
 const adminController = require('../controllers/admin.controller');
 
 // Apply authentication middleware to all admin routes
@@ -17,6 +19,9 @@ router.post('/products', isAdmin, addProduct);
 router.put('/products/:id', isAdmin, modifyProductDetails);
 router.patch('/products/:id/status', isAdmin, toggleProductStatus);
 router.put('/products/:id/customization', isAdmin, updateProductCustomization);
+
+// Admin user routes
+router.post('/register', isAdmin, validateRegister, registerAdmin);
 
 // New route for uploading product images
 router.post('/products/:id/image', isAdmin, handleProductImageUpload, uploadProductImage);
