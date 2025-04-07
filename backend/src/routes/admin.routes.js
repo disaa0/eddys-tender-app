ce5d426742e5e213c061919c9f68f9611ef81ba5
San <85775871+disaa0@users.noreply.github.com>
2025-04-06 23:13:53 -0700

    Admin Endpoint: Patch order status + Docs

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -8,7 +8,7 @@ const { handleProductImageUpload } = require('../middlewares/upload.middleware')
 const { validateRegister } = require('../middlewares/validateInput');
 const { registerAdmin } = require('../controllers/auth.controller');
 const adminController = require('../controllers/admin.controller');
-const { getOrdersByDateRange } = require('../controllers/adminOrder.controller');
+const { getOrdersByDateRange, updateOrderStatus } = require('../controllers/adminOrder.controller');
 
 // Apply authentication middleware to all admin routes
 router.use(authenticateToken);
@@ -26,6 +26,7 @@ router.post('/register', isAdmin, validateRegister, registerAdmin);
 
 // Admin order routes
 router.get('/orders', isAdmin, getOrdersByDateRange);
+router.patch('/order/:id', isAdmin, updateOrderStatus);
 
 // New route for uploading product images
 router.post('/products/:id/image', isAdmin, handleProductImageUpload, uploadProductImage);
