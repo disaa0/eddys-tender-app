83a10619855a6392bfeae268fd642210450084f6
Daniel Ulloa <ulloadaniel2101@gmail.com>
2025-05-11 16:08:50 -0700

    Se implementa el refresh en el detalle de las ordenes activas en el apartado del admin

--- frontend/app/(appAdmin)/orders/[id].js
+++ frontend/app/(appAdmin)/orders/[id].js
@@ -13,6 +13,8 @@ import { ActivityIndicator, Card, Chip, IconButton, List, RadioButton } from 're
 import { useAdminOrders } from '../../hooks/useAdminOrders';
 import adminApiService from '../../api/AdminApiService';
 import ConfirmationDialog from '../../components/ConfirmationDialog';
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback } from 'react';
 
 const OrderDetail = () => {
   const router = useRouter();
@@ -61,9 +63,11 @@ const OrderDetail = () => {
     }
   };
 
-  useEffect(() => {
-    if (id) getOrderById(id);
-  }, [id]);
+  useFocusEffect(
+    useCallback(() => {
+      if (id) getOrderById(id);
+    }, [id])
+  );
 
   const handleStatusChange = async (newStatus) => {
     console.log(`Updating order status to: ${newStatus}`);
